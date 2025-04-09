f1b1b4c898f77f7755f8b13506aa41fec5ac0687
Celine Lee <celine.y.lee@gmail.com>
2025-04-09 18:01:23 -0400

    o3 mini zero shot

--- wcwidth/wcwidth.py
+++ wcwidth/wcwidth.py
@@ -1,3 +1,4 @@
+
 """
 This is a python implementation of wcwidth() and wcswidth().
 
@@ -85,6 +86,33 @@ except ImportError:
 _PY3 = sys.version_info[0] >= 3
 
 
+def search_table(ucs, range_table):
+    """
+    Helper function to determine if a Unicode code point (ucs)
+    lies within any of the ranges specified in the sorted range_table.
+
+    :param int ucs: Unicode code point.
+    :param list range_table: List of (start, end) tuples.
+    :returns: True if ucs is found in one of the intervals, False otherwise.
+    """
+    if not range_table:
+        return False
+    if ucs < range_table[0][0] or ucs > range_table[-1][1]:
+        return False
+    low = 0
+    high = len(range_table) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        start, end = range_table[mid]
+        if ucs < start:
+            high = mid - 1
+        elif ucs > end:
+            low = mid + 1
+        else:
+            return True
+    return False
+
+
 @lru_cache(maxsize=1000)
 def wcwidth(wc, unicode_version='auto'):
     r"""
@@ -92,7 +120,7 @@ def wcwidth(wc, unicode_version='auto'):
 
     :param str wc: A single Unicode character.
     :param str unicode_version: A Unicode version number, such as
-        ``'6.0.0'``. A list of version levels suported by wcwidth
+        ``'6.0.0'``. A list of version levels supported by wcwidth
         is returned by :func:`list_versions`.
 
         Any version string may be specified without error -- the nearest
@@ -110,46 +138,23 @@ def wcwidth(wc, unicode_version='auto'):
     """
     ucs = ord(wc) if wc else 0
 
-    # small optimization: early return of 1 for printable ASCII, this provides
-    # approximately 40% performance improvement for mostly-ascii documents, with
-    # less than 1% impact to others.
+    # Small optimization: early return for printable ASCII.
     if 32 <= ucs < 0x7f:
         return 1
 
-    # C0/C1 control characters are -1 for compatibility with POSIX-like calls
-    if ucs and ucs < 32 or 0x07F <= ucs < 0x0A0:
+    # C0/C1 control characters return -1 for compatibility.
+    if (ucs and ucs < 32) or (0x07F <= ucs < 0x0A0):
         return -1
 
-    _unicode_version = _wcmatch_version(unicode_version)
+    _uv = _wcmatch_version(unicode_version)
 
-    # Zero width
-    lbound = 0
-    ubound = len(ZERO_WIDTH[_unicode_version]) - 1
+    # Check zero width characters.
+    if search_table(ucs, ZERO_WIDTH[_uv]):
+        return 0
 
-    if not (ucs < ZERO_WIDTH[_unicode_version][0][0] or ucs > ZERO_WIDTH[_unicode_version][ubound][1]):
-        while ubound >= lbound:
-            mid = (lbound + ubound) // 2
-            if ucs > ZERO_WIDTH[_unicode_version][mid][1]:
-                lbound = mid + 1
-            elif ucs < ZERO_WIDTH[_unicode_version][mid][0]:
-                ubound = mid - 1
-            else:
-                return 0
-
-    # 1 or 2 width
-    lbound = 0
-    ubound = len(WIDE_EASTASIAN[_unicode_version]) - 1
-
-    if ucs < WIDE_EASTASIAN[_unicode_version][0][0] or ucs > WIDE_EASTASIAN[_unicode_version][ubound][1]:
-        return 1
-    while ubound >= lbound:
-        mid = (lbound + ubound) // 2
-        if ucs > WIDE_EASTASIAN[_unicode_version][mid][1]:
-            lbound = mid + 1
-        elif ucs < WIDE_EASTASIAN[_unicode_version][mid][0]:
-            ubound = mid - 1
-        else:
-            return 2
+    # Check for wide characters.
+    if search_table(ucs, WIDE_EASTASIAN[_uv]):
+        return 2
 
     return 1
 
@@ -175,68 +180,56 @@ def wcswidth(pwcs, n=None, unicode_version='auto'):
 
     See :ref:`Specification` for details of cell measurement.
     """
-    # this 'n' argument is a holdover for POSIX function
+    # 'n' is a holdover for POSIX function signature.
     _unicode_version = None
     end = len(pwcs) if n is None else n
     width = 0
     idx = 0
     last_measured_char = None
+
     while idx < end:
         char = pwcs[idx]
         if char == u'\u200D':
-            # Zero Width Joiner, do not measure this or next character
+            # Zero Width Joiner: skip this and the next character.
             idx += 2
             continue
+
         if char == u'\uFE0F' and last_measured_char:
-            # on variation selector 16 (VS16) following another character,
-            # conditionally add '1' to the measured width if that character is
-            # known to be converted from narrow to wide by the VS16 character.
+            # Variation Selector-16: adjust width conditionally.
             if _unicode_version is None:
                 _unicode_version = _wcversion_value(_wcmatch_version(unicode_version))
             if _unicode_version >= (9, 0, 0):
                 ucs = ord(last_measured_char)
-                lbound = 0
-                ubound = len(VS16_NARROW_TO_WIDE["9.0.0"]) - 1
-
-                if not (ucs < VS16_NARROW_TO_WIDE["9.0.0"][0][0] or ucs > VS16_NARROW_TO_WIDE["9.0.0"][ubound][1]):
-                    while ubound >= lbound:
-                        mid = (lbound + ubound) // 2
-                        if ucs > VS16_NARROW_TO_WIDE["9.0.0"][mid][1]:
-                            lbound = mid + 1
-                        elif ucs < VS16_NARROW_TO_WIDE["9.0.0"][mid][0]:
-                            ubound = mid - 1
-                        else:
-                            width += 1
-                            break
-
-                last_measured_char = None
+                if search_table(ucs, VS16_NARROW_TO_WIDE["9.0.0"]):
+                    width += 1
+            last_measured_char = None
             idx += 1
             continue
-        # measure character at current index
+
+        # Compute width of the current character.
         wcw = wcwidth(char, unicode_version)
         if wcw < 0:
-            # early return -1 on C0 and C1 control characters
+            # Early return -1 on C0 and C1 control characters.
             return wcw
         if wcw > 0:
-            # track last character measured to contain a cell, so that
-            # subsequent VS-16 modifiers may be understood
+            # Track last non-zero-width character for potential variation selectors.
             last_measured_char = char
         width += wcw
         idx += 1
+
     return width
 
 
 @lru_cache(maxsize=128)
 def _wcversion_value(ver_string):
     """
-    Integer-mapped value of given dotted version string.
+    Integer-mapped value of a dotted version string.
 
-    :param str ver_string: Unicode version string, of form ``n.n.n``.
-    :rtype: tuple(int)
-    :returns: tuple of digit tuples, ``tuple(int, [...])``.
+    :param str ver_string: Unicode version string of form ``n.n.n``.
+    :rtype: tuple
+    :returns: Tuple of integers, e.g. (n, n, n).
     """
-    retval = tuple(map(int, (ver_string.split('.'))))
-    return retval
+    return tuple(map(int, ver_string.split('.')))
 
 
 @lru_cache(maxsize=8)
@@ -244,8 +237,8 @@ def _wcmatch_version(given_version):
     """
     Return nearest matching supported Unicode version level.
 
-    If an exact match is not determined, the nearest lowest version level is
-    returned after a warning is emitted.  For example, given supported levels
+    If an exact match is not determined, the nearest lower version level is
+    returned after a warning is emitted. For example, given supported levels
     ``4.1.0`` and ``5.0.0``, and a version string of ``4.9.9``, then ``4.1.0``
     is selected and returned:
 
@@ -256,30 +249,20 @@ def _wcmatch_version(given_version):
     >>> _wcmatch_version('1')
     '4.1.0'
 
-    :param str given_version: given version for compare, may be ``auto``
-        (default), to select Unicode Version from Environment Variable,
+    :param str given_version: Given version for comparison. May be ``auto``
+        (default) to select the Unicode version from the environment variable
         ``UNICODE_VERSION``. If the environment variable is not set, then the
-        latest is used.
+        latest available version is used.
     :rtype: str
-    :returns: unicode string, or non-unicode ``str`` type for python 2
-        when given ``version`` is also type ``str``.
+    :returns: A Unicode version string.
     """
-    # Design note: the choice to return the same type that is given certainly
-    # complicates it for python 2 str-type, but allows us to define an api that
-    # uses 'string-type' for unicode version level definitions, so all of our
-    # example code works with all versions of python.
-    #
-    # That, along with the string-to-numeric and comparisons of earliest,
-    # latest, matching, or nearest, greatly complicates this function.
-    # Performance is somewhat curbed by memoization.
-    _return_str = not _PY3 and isinstance(given_version, str)
+    _return_str = (not _PY3) and isinstance(given_version, str)
 
     if _return_str:
-        # avoid list-comprehension to work around a coverage issue:
-        # https://github.com/nedbat/coveragepy/issues/753
         unicode_versions = list(map(lambda ucs: ucs.encode(), list_versions()))
     else:
         unicode_versions = list_versions()
+
     latest_version = unicode_versions[-1]
 
     if given_version in (u'auto', 'auto'):
@@ -288,38 +271,25 @@ def _wcmatch_version(given_version):
             'latest' if not _return_str else latest_version.encode())
 
     if given_version in (u'latest', 'latest'):
-        # default match, when given as 'latest', use the most latest unicode
-        # version specification level supported.
         return latest_version if not _return_str else latest_version.encode()
 
     if given_version in unicode_versions:
-        # exact match, downstream has specified an explicit matching version
-        # matching any value of list_versions().
         return given_version if not _return_str else given_version.encode()
 
-    # The user's version is not supported by ours. We return the newest unicode
-    # version level that we support below their given value.
     try:
         cmp_given = _wcversion_value(given_version)
-
     except ValueError:
-        # submitted value raises ValueError in int(), warn and use latest.
         warnings.warn("UNICODE_VERSION value, {given_version!r}, is invalid. "
-                      "Value should be in form of `integer[.]+', the latest "
+                      "Value should be in form of `integer[.]+'. The latest "
                       "supported unicode version {latest_version!r} has been "
                       "inferred.".format(given_version=given_version,
                                          latest_version=latest_version))
         return latest_version if not _return_str else latest_version.encode()
 
-    # given version is less than any available version, return earliest
-    # version.
     earliest_version = unicode_versions[0]
     cmp_earliest_version = _wcversion_value(earliest_version)
 
     if cmp_given <= cmp_earliest_version:
-        # this probably isn't what you wanted, the oldest wcwidth.c you will
-        # find in the wild is likely version 5 or 6, which we both support,
-        # but it's better than not saying anything at all.
         warnings.warn("UNICODE_VERSION value, {given_version!r}, is lower "
                       "than any available unicode version. Returning lowest "
                       "version level, {earliest_version!r}".format(
@@ -327,30 +297,16 @@ def _wcmatch_version(given_version):
                           earliest_version=earliest_version))
         return earliest_version if not _return_str else earliest_version.encode()
 
-    # create list of versions which are less than our equal to given version,
-    # and return the tail value, which is the highest level we may support,
-    # or the latest value we support, when completely unmatched or higher
-    # than any supported version.
-    #
-    # function will never complete, always returns.
     for idx, unicode_version in enumerate(unicode_versions):
-        # look ahead to next value
         try:
             cmp_next_version = _wcversion_value(unicode_versions[idx + 1])
         except IndexError:
-            # at end of list, return latest version
             return latest_version if not _return_str else latest_version.encode()
 
-        # Maybe our given version has less parts, as in tuple(8, 0), than the
-        # next compare version tuple(8, 0, 0). Test for an exact match by
-        # comparison of only the leading dotted piece(s): (8, 0) == (8, 0).
         if cmp_given == cmp_next_version[:len(cmp_given)]:
             return unicode_versions[idx + 1]
 
-        # Or, if any next value is greater than our given support level
-        # version, return the current value in index.  Even though it must
-        # be less than the given value, its our closest possible match. That
-        # is, 4.1 is returned for given 4.9.9, where 4.1 and 5.0 are available.
         if cmp_next_version > cmp_given:
             return unicode_version
-    assert False, ("Code path unreachable", given_version, unicode_versions)  # pragma: no cover
+
+    assert False, ("Code path unreachable", given_version, unicode_versions)
